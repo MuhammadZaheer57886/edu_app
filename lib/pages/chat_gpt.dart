7a78f66df850e3d8027a2e2d4521d6b5117d861a
MuhammadZaheer57886 <mza57886@gmail.com>
2023-02-09 16:15:00 +0500

    sec

--- lib/pages/chat_gpt.dart
+++ lib/pages/chat_gpt.dart
@@ -13,7 +13,7 @@ class ChatGpt extends StatelessWidget {
           
           appBar: AppBar(
             backgroundColor: Colors.redAccent,
-            title: const AnimatedText('Chat with GPT'),
+            title: const Text('Chat with GPT'),
           ),
           body: const Center(
             child: Text('Hello World'),
@@ -24,43 +24,3 @@ class ChatGpt extends StatelessWidget {
   }
 }
 
-class AnimatedText extends StatefulWidget {
-  final String text;
-
-  const AnimatedText(this.text, {super.key});
-
-  @override
-  AnimatedTextState createState() => AnimatedTextState();
-}
-
-class AnimatedTextState extends State<AnimatedText>
-    with TickerProviderStateMixin {
-  late AnimationController _controller;
-  late Animation<double> _animation;
-
-  @override
-  void initState() {
-    super.initState();
-
-    _controller =
-        AnimationController(duration: const Duration(seconds: 1), vsync: this);
-
-    _animation = Tween<double>(begin: 0, end: 1).animate(_controller);
-
-    _controller.forward();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return FadeTransition(
-      opacity: _animation,
-      child: Text(
-        widget.text,
-        style: const TextStyle(
-          fontSize: 20,
-          color: Colors.white,
-        ),
-      ),
-    );
-  }
-}
